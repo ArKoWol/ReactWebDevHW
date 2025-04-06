4f9d14a74ff09e2983e045b5bffeef20eab9b3c6
Artsiom <artsiom.yasiukou@student.ehu.lt>
2025-04-06 22:04:13 +0300

    style: [#2] style code with prettier

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -9,22 +9,18 @@ import React from 'react';
 export function Navbar() {
 	return (
 		<div className="navbar">
-			<div className='logo'>
+			<div className="logo">
 				<img src={logo} alt="Logo" />
 			</div>
-			<div className='navigation'>
+			<div className="navigation">
 				{options.map((item, index) => (
 					<div key={index} className="radio-item">
-						<input
-							type="radio"
-							id={item.id}
-							name="radio-group"
-						/>
+						<input type="radio" id={item.id} name="radio-group" />
 						<label htmlFor={item.id}>{item.label}</label>
 					</div>
 				))}
 				<Button>
-					<img src={cart} alt=""/>
+					<img src={cart} alt="" />
 				</Button>
 			</div>
 		</div>
