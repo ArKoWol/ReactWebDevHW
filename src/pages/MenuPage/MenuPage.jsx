5e8615cc7e138f2f994412a525de05bb6e715419
Artsiom <artsiom.yasiukou@student.ehu.lt>
2025-04-07 20:02:30 +0300

    feature: [#2] add menu page

---
+++ src/pages/MenuPage/MenuPage.jsx
@@ -0,0 +1,46 @@
+import './MenuPage.css';
+import { Button } from '../../components/Button/Button.jsx';
+import { menuTypes } from '../../data/menuTypes.js';
+import React from 'react';
+import { cardsData } from '../../data/cardsData.js';
+import { MenuCard } from '../../components/MenuCard/MenuCard.jsx';
+
+export function MenuPage() {
+	return (
+		<div className="menu-page">
+			<div className="triangle"></div>
+			<div className="title">
+				<h1>Browse our menu</h1>
+				<p>
+					Use our menu to place an order online, or <span>phone</span> our store
+					to place a pickup order. Fast and fresh food.
+				</p>
+				<div className="menu-select">
+					{menuTypes.map((item, index) => (
+						<div key={index} className="radio-item">
+							<input
+								type="radio"
+								id={item.id}
+								name="menus-radio-group"
+								onChange={() => handleChange(item.id)}
+							/>
+							<label htmlFor={item.id}>{item.label}</label>
+						</div>
+					))}
+				</div>
+			</div>
+			<div className="menu-content">
+				{cardsData.slice(0, 6).map((card, index) => (
+					<MenuCard
+						key={index}
+						image={card.image}
+						title={card.title}
+						price={card.price}
+						description={card.description}
+					/>
+				))}
+			</div>
+			<Button>See more</Button>
+		</div>
+	);
+}
