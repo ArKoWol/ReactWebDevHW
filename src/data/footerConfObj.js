e9494f9901d2fcc6ced0ac6d614c46216507b396
Artsiom <artsiom.yasiukou@student.ehu.lt>
2025-04-13 20:17:08 +0300

    fix: [#2] adding footer configuration object

---
+++ src/data/footerConfObj.js
@@ -0,0 +1,14 @@
+export const footerSections = [
+	{
+		title: 'COMPANY',
+		links: ['Home', 'Order', 'FAQ', 'Contact'],
+	},
+	{
+		title: 'TEMPLATE',
+		links: ['Style Guide', 'Changelog', 'Licence', 'Webflow University'],
+	},
+	{
+		title: 'FLOWBASE',
+		links: ['More Cloneables'],
+	},
+];
