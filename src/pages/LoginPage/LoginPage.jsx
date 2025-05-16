529edc5dff27300a7d5c023b1d2cdb024c93ae33
Artsiom <artsiom.yasiukou@student.ehu.lt>
2025-05-16 11:54:57 +0300

    feature: [#6] implement login page

--- src/pages/LoginPage/LoginPage.jsx
+++ src/pages/LoginPage/LoginPage.jsx
@@ -1,5 +1,72 @@
 import './LoginPage.css';
+import React, { useState } from 'react';
+import { Button } from '../../components/Button/Button.jsx';
+import { auth } from '../../firebase/firebaseApp';
+import { signInWithEmailAndPassword } from 'firebase/auth';
+import { useNavigate } from 'react-router-dom';
+
+
+async function loginUser(email, password, navigate) {
+	try {
+		const userCredential = await signInWithEmailAndPassword(auth, email, password);
+		console.log('Log in success:', userCredential.user.email);
+		alert(`Welcome, ${userCredential.user.email}`);
+		navigate('/');
+	}
+	catch (err) {
+		console.error('Log in Error:', err.message);
+		alert(`Log in Error: ${err.message}`);
+	}
+}
 
 export function LoginPage() {
-	return <p>this is your Login</p>;
+	const [email, setEmail] = useState('');
+	const [password, setPassword] = useState('');
+	const navigate = useNavigate();
+
+	const handleSubmit = (e) => {
+		e.preventDefault();
+		loginUser(email, password, navigate);
+	};
+
+	return (
+		<div className="LoginPage">
+			<div className="triangle"></div>
+			<div className="login-page-container">
+				<h1>Log in</h1>
+				<form className="login-form" onSubmit={handleSubmit}>
+					<div className="login-form-input-container">
+						<p>User name</p>
+						<div className="login-form-input">
+							<input
+								type="text"
+								placeholder="Email"
+								value={email}
+								onChange={(e) => setEmail(e.target.value)}
+								required
+							/>
+						</div>
+					</div>
+					<div className="login-form-input-container">
+						<p>Password</p>
+						<div className="login-form-input">
+							<input
+								type="password"
+								placeholder="Password"
+								value={password}
+								onChange={(e) => setPassword(e.target.value)}
+								required
+							/>
+						</div>
+					</div>
+					<div className="login-form-button-container">
+						<Button type="submit">Submit</Button>
+						<Button type="button" onClick={() => { setEmail(''); setPassword(''); }}>
+							Cancel
+						</Button>
+					</div>
+				</form>
+			</div>
+		</div>
+	);
 }
