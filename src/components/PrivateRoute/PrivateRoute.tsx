310220693b099a06f54200e0427208f8f81699b2
Artsiom <artsiom.yasiukou@student.ehu.lt>
2025-06-15 13:49:20 +0300

    feature [#9]: Implement Routing

---
+++ src/components/PrivateRoute/PrivateRoute.tsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import { Navigate } from 'react-router-dom';
+import { useAppSelector } from '../../store/hooks';
+import type { RootState } from '../../store';
+
+interface PrivateRouteProps {
+	children: React.ReactNode;
+}
+
+export function PrivateRoute({ children }: PrivateRouteProps): React.ReactElement {
+	const { currentUser, loading } = useAppSelector((state: RootState) => state.auth);
+
+	if (loading) {
+		return <div>Loading...</div>;
+	}
+
+	if (!currentUser) {
+		return <Navigate to="/login" replace />;
+	}
+
+	return <>{children}</>;
+} 
